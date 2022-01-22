34b3182e26354c17fce38e9a6f0dddf920ad5b37
hagin145 <brawlmini2001@gmail.com>
2022-01-22 13:01:57 +0400

    first commit

---
+++ cv1/javascript/arrays.js
@@ -0,0 +1,54 @@
+
+
+// //arrays are reference datatypes
+// //predifined functions for arrays
+// // 1) console.log
+// // 2) push
+// // 3) map
+
+
+
+
+// let fruit = 'apple'
+// let fruit1 = 'orange'
+// let fruits = ['apple', 'orange', 'banana']
+
+// // fruits[3] = 'melon'  //adding new values in the array
+  
+
+// //console.log is a predefined function only used for arrays
+
+// // fruits.push('grapes')
+// // console.log(fruits)
+
+// //String addition or concatination
+
+// // fruits = fruits + 's'
+// // console.log(fruits)
+// let item = 'apple'
+// let updatedFruits = fruits.map((item) => {
+//     updatedfruits = item + 's'
+//     return updatedfruits;
+// })
+// console.log('updatedFruits', updatedFruits)
+// console.log(fruits)
+
+
+// let newArray = []
+// fruits.forEach(element => {
+//     newArray.push(element + 's')
+// })
+// console.log(fruits)
+// console.log(newArray)
+
+
+
+function calling(){
+    fetch('http://localhost:3000/home', {
+        method: 'GET', // or 'PUT'
+      }) 
+}
+
+
+
+
